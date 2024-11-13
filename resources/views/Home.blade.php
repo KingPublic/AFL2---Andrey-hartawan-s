695f45dfbc2aa3804243d8b4634663bc12e8974f
Andrey Hartawan Suwardi <144876604+KingPublic@users.noreply.github.com>
2024-11-13 15:48:36 +0800

    tes lagi

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>My Project Portfolioo</title>
     <link rel="icon" type="image/x-icon" href="/assets/fav-icon.jpg" />
-    <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('asset/css/style.css') }}" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
