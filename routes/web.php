2776e09a4bd872ffb914ca98b1688dd5ea70f339
Andrey Hartawan Suwardi <144876604+KingPublic@users.noreply.github.com>
2024-11-13 15:34:52 +0800

    updateV5

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//error karena php ku beda versi jdi nda bisa composer install 
+//error karena php ku beda versi jdi nda bisa composer install  //berhasilmi
 Route::get('/', function () {
     return view('Home');
 });
